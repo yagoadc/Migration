d2baf9773cdf85fb50fe9c61ae08cd260b9236b9
Vitor <vitorofa@gmail.com>
2016-09-22 18:09:23 -0300

    *formatação tabela

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -15,6 +15,15 @@ class ProductsController extends Controller
 
         $products = Product::all();
 
+        foreach($products as $p){
+            //var_dump($p);
+            $str_tokens = explode('.',strval($p->preco));
+            if(strlen($str_tokens[1]) == 1){
+                $str_tokens[1] = $str_tokens[1]."0";
+            }
+            $p->merge(['preco' => implode(',',$str_tokens)]);
+        }
+
         return view('produtos',['products' => $products]);
     }
 }
--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -5,17 +5,22 @@
         <table class="table table-striped">
             <thead>
                 <tr class="row">
-                    <th class="col-xs-6">Fornecedor</th>
-                    <th class="col-xs-1">Produto</th>
-                    <th class="col-xs-5 text-right">Qtde. em Estoque</th>
+                    <th class="col-xs-3">Fornecedor</th>
+                    <th class="col-xs-3">Produto</th>
+                    <th class="col-xs-3">Qtde. em Estoque</th>
+                    <th class="col-xs-3 text-right">Preço</th>
                 </tr>
             </thead>
             <tbody>
             @foreach($products as $p)
                 <tr id="{{"product".$p->product_id}}" class="row">
-                    <td class="col-xs-6">{{"Empresa".$loop->iteration}}{{--}}{{$p->provider->nome}}--}}</td>
-                    <td class="col-xs-1">{{$p->nome}}</td>
-                    <td class="col-xs-5 text-right">{{$p->estoque}}</td>
+                    <td class="col-xs-3">{{"Empresa".$loop->iteration}}{{--}}{{$p->provider->nome}}--}}</td>
+                    <td class="col-xs-4">{{$p->nome}}</td>
+                    <td class="col-xs-2">{{$p->estoque}}</td>
+                    {{
+
+                    }}
+                    <td class="col-xs-3 text-right">{{"R$ ".$p->preco}}</td>
                 </tr>
             @endforeach
             </tbody>
