43a8ff5da11efe6e79168c82b64f2cd2d4f159f9
vitor25 <vitorofa@gmail.com>
2016-09-26 19:51:11 -0300

    *StudentsTableSeeder

---
+++ database/seeds/StudentsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->truncate();
+        factory(App\Student::class, 20)->create();
+    }
+}
